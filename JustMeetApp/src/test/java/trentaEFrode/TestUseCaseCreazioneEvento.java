8df271ff9d8a3a07003b28b10bb0a6b76525f341
Veronica <veronica.pignotti@studenti.unicam.it>
2020-02-10 16:18:19 +0100

    Prova commit creazione testCreazione evento

---
+++ JustMeetApp/src/test/java/trentaEFrode/TestUseCaseCreazioneEvento.java
@@ -0,0 +1,39 @@
+package trentaEFrode;
+
+//import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.ConnectException;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import unicam.trentaEFrode.domain.mainElements.Categoria;
+import unicam.trentaEFrode.domain.mainElements.Evento;
+import unicam.trentaEFrode.domain.mainElements.GestoreEventi;
+import unicam.trentaEFrode.domain.mainElements.Luogo;
+import unicam.trentaEFrode.domain.users.UtenteRegistrato;
+
+class TestUseCaseCreazioneEvento {
+
+	@Test
+	void testEventoValido() {
+		UtenteRegistrato organizzatore = UtenteRegistrato.getInstance().id(18);
+				GregorianCalendar data = new GregorianCalendar();
+				data.add(GregorianCalendar.MONTH, 2);
+				Evento e = new Evento("Pranzo di beneficenza", data, 3, 100, "I soldi ricavati andranno in beneficenza", 3, new Luogo("Ristorante Casa Mia", "via della cucina", "15/A", "63100", "Ascoli Piceno", "AP"), new Categoria(4, "Cibo", "Per le persone più golose."));
+				e = e.setOrganizzatore(organizzatore.getId());
+				List<Integer> risposta;
+				try {
+					risposta = GestoreEventi.getInstance().effettuaControlli(e);
+				
+				assertEquals(1, risposta.size());
+				assertEquals(new Integer(-1), risposta.get(0));
+				} catch (ConnectException e1) {
+					e1.printStackTrace();
+				}
+		}
+
+}
